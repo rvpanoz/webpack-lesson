0c0663b0389b8ef3bea902ae1a26837c5aac58a2
rvpanoz <rvpanoz@gmail.com>
2019-10-08 22:38:36 +0300

    setup dependencies and webpack

---
+++ configs/webpack.config.base.js
@@ -0,0 +1,14 @@
+/**
+ * Base webpack config used across other specific configs
+ */
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+};
