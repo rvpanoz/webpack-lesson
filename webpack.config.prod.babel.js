f1f56350079d3b79726dc32643b38954d50550e4
rvpanoz <rvpanoz@gmail.com>
2019-10-15 16:17:37 +0300

    chore: add more comments

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -1,70 +1,49 @@
 /**
- * webpack dev configuration
+ * webpack production configuration
  */
 
-const path = require('path');
-const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
-const port = process.env.PORT || 5599;
+const prodConfig = {
+    mode: 'production',
+    devtool: 'inline-source-map',
+    entry: ['./src/index.js'], // string | object | array
 
-module.exports = {
-    // Chosen mode tells webpack to use its built-in optimizations accordingly.
-    mode: 'development', // "production" | "development" | "none"
-
-    // enhance debugging by adding meta info for the browser devtools
-    // source-map most detailed at the expense of build speed.
-    devtool: 'inline-source-map', // enum
-
-    // defaults to ./src   
-    entry: './src/index.js', // string | object | array
-
-    // Here the application starts executing and webpack starts bundling 
+    // manually code splitting using entry points
+    // entry: {
+    //     index: './src/index',
+    //     anotherEntry: './src/components/Entry'
+    // },
+    
     output: {
-        filename: '[name].bundle.js', // the filename template for entry chunks
-        path: path.resolve(__dirname, 'dist'), // the target directory for all output files - must be an absolute path (use the Node.js path module)
-        publicPath: '/' // the url to the output directory resolved relative to the HTML page
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        chunkFilename: '[name].bundle.js',
+        publicPath: '/'
     },
-
-    // development server configuration
-    devServer: {
-        contentBase: './dist', // boolean | string | array, static file locati
-        port,
-        hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
-        noInfo: true, // only errors & warns on hot reload
-        historyApiFallback: true // true for index.html upon 404, object for multiple paths
-    },
+    performance: { hints: false },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
         }),
 
-        new BundleAnalyzerPlugin(),
-
         new CleanWebpackPlugin()
     ],
-
-    // options for resolving module requests
     resolve: {
-        // a list of module name aliases
         alias: {
             components: path.resolve(path.join(__dirname, 'src', 'components')),
             images: path.resolve(path.join(__dirname, 'src', 'assets', 'images')),
             styles: path.resolve(path.join(__dirname, 'src', 'assets', 'styles')),
         },
     },
-
-    // configuration regarding modules
     module: {
         rules: [
-            // rules for modules (configure loaders, parser options, etc.)
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
-                    // the loader which should be applied, it'll be resolved relative to the context
                     loader: "babel-loader"
                 }
             },
@@ -127,5 +106,21 @@ module.exports = {
             }
         ],
     },
-    performance: { hints: 'warning' }
+    // optimization
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    // vendor chunk
+                    name: "vendor",
+                    // sync + async chunks
+                    chunks: 'all',
+                    // import file path containing node_modules
+                    test: /node_modules/
+                }
+            }
+        }
+    }
 };
+
+export default prodConfig
