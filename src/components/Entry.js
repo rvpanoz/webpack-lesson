ca6eb06bb117be18b6b6fe08f66542aff69b7c4f
rvpanoz <rvpanoz@gmail.com>
2019-10-15 17:10:06 +0300

    fix: components index

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,11 +1,1 @@
-import React from 'react'
-
-const Entry = () => {
-    return (
-        <div className={styles['article-text']}>
-            <h2>An entry point</h2>
-        </div>
-    )
-}
-
-export default Entry
+export { Entry as default } from './imports';
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,30 +1,37 @@
-import React, { Suspense, lazy } from 'react'
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ErrorBoundary from './ErrorBoundary'
+import ErrorBoundary from './ErrorBoundary';
 
 // this component will not code splitted
-import AppLoader from 'components/AppLoader'
+import { AppLoader } from 'components/imports';
 
 // load css as a webpack modules
 import styles from 'styles/app.css';
 
-// lazy does not support named export
-const Home = lazy(() => import(/* webpackChunkName: "Home" */'components/Home'));
-const Page = lazy(() => import(/* webpackChunkName: "Page" */'components/Page'));
+// React.lazy currently only supports default exports. If the module you want to import uses named exports,
+// we have to create an intermediate module that reexports it as the default.
+const Home = lazy(() =>
+  import(/* webpackChunkName: "Home" */ 'components/Home')
+);
+const Page = lazy(() =>
+  import(/* webpackChunkName: "Page" */ 'components/Page')
+);
 
-const App = () => <div className={styles.app}>
+const App = () => (
+  <div className={styles.app}>
     <ErrorBoundary>
-        <Suspense fallback={<AppLoader />}>
-            <div className={styles.content}>
-                <Router>
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route path="/page" component={Page} />
-                    </Switch>
-                </Router>
-            </div>
-        </Suspense>
+      <Suspense fallback={<AppLoader />}>
+        <div className={styles.content}>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/page" component={Page} />
+            </Switch>
+          </Router>
+        </div>
+      </Suspense>
     </ErrorBoundary>
-</div>
+  </div>
+);
 
 export default App;
